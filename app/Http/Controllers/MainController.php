1030cdc794e6fc2fe4a93e466be2fbda5326155a
Duc Nguyen <ducntq@gmail.com>
2015-10-21 23:01:42 +0700

    Initial commit with gulp, bower

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ducntq
+ * Date: 21/10/2015
+ * Time: 23:00
+ */
+
+namespace App\Http\Controllers;
+
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        return view('main');
+    }
+}
---
+++ resources/views/main.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GeoTweet - A project by Duc Nguyen</title>
+    </head>
+    <body>
+    @yield('content')
+    </body>
+</html>
