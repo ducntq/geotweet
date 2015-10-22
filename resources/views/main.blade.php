5d916d634216e42adcfcefef8f174179bd8d3583
Duc Nguyen <ducntq@gmail.com>
2015-10-22 22:06:11 +0700

    Improve responsive layout

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,11 +5,20 @@
             <div id="map"></div>
         </div>
         <div class="row no-gutters" id="controls">
-            <div class="col-md-8">
+            <div class="col-md-8 col-sm-12 col-xs-12">
                 <input type="text" class="form-control input-lg" placeholder="Enter a city to begin" />
             </div>
-            <div class="col-md-2"><button class="btn btn-primary btn-block btn-lg">Search</button></div>
-            <div class="col-md-2"><button class="btn btn-warning btn-block btn-lg">History</button></div>
+            <div class="col-md-2 col-sm-6 col-xs-6"><button class="btn btn-primary btn-block btn-lg">Search</button></div>
+            <div class="col-md-2 col-sm-6 col-xs-6">
+                <div class="btn-group dropup btn-block">
+                    <button type="button" class="btn btn-default dropdown-toggle btn-lg btn-block" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        History <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Empty</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
