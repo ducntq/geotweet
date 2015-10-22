ccc2b0c60bb071008fc1ec07395f1c697ec54433
Duc Nguyen <ducntq@gmail.com>
2015-10-22 23:53:30 +0700

    Improve responsive layout

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -2,6 +2,54 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 12.19070471984058, lng: 94.394},
-        zoom: 2
+        zoom: 3
     });
+
+    // init new GeoTweet instance and bind events
+    var geoTweet = new GeoTweet(jQuery, map);
+    geoTweet.bind();
 }
+
+var GeoTweet = (function () {
+    var $, map, $input, $searchBtn, $historyBtn;
+
+    /**
+     * Constructor of GeoTweet
+     *
+     * Init all jQuery objects
+     *
+     * @param jQuery
+     * @param googleMap
+     * @constructor
+     */
+    function GeoTweet (jQuery, googleMap) {
+        $ = jQuery;
+        map = googleMap;
+
+        $input = $('#txtInput');
+        $searchBtn = $('#search-btn');
+        $historyBtn = $('#history-btn');
+    }
+
+    /**
+     * Bind click event on Search button
+     */
+    function bindSearchBtn() {
+        $searchBtn.off().on('click', function(e) {
+            e.preventDefault();
+        });
+    }
+
+    function search(query, successCallback, failureCallback) {
+
+    }
+
+    /**
+     * Expose public `bind` method
+     */
+    GeoTweet.prototype.bind = function () {
+        bindSearchBtn();
+    };
+
+    return GeoTweet;
+})();
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,18 +6,13 @@
         </div>
         <div class="row no-gutters" id="controls">
             <div class="col-md-8 col-sm-12 col-xs-12">
-                <input type="text" class="form-control input-lg" placeholder="Enter a city to begin" />
+                <input type="text" id="txtInput" class="form-control input-lg" placeholder="Enter a city name to begin" />
             </div>
-            <div class="col-md-2 col-sm-6 col-xs-6"><button class="btn btn-primary btn-block btn-lg">Search</button></div>
             <div class="col-md-2 col-sm-6 col-xs-6">
-                <div class="btn-group dropup btn-block">
-                    <button type="button" class="btn btn-default dropdown-toggle btn-lg btn-block" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        History <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Empty</a></li>
-                    </ul>
-                </div>
+                <button id="search-btn" class="btn btn-primary btn-block btn-lg">Search</button>
+            </div>
+            <div class="col-md-2 col-sm-6 col-xs-6">
+                <button id="history-btn" class="btn btn-default btn-block btn-lg">History</button>
             </div>
         </div>
     </div>
