32a60970deff3b452cb8a504e65599e7f9f88703
Duc Nguyen <ducntq@gmail.com>
2015-10-23 02:06:38 +0700

    Pan and zoom on city

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -4,7 +4,7 @@ function initMap() {
         center: {lat: 12.19070471984058, lng: 94.394},
         zoom: 3
     });
-
+    console.log('Map initialized');
     // init new GeoTweet instance and bind events
     var geoTweet = new GeoTweet(jQuery, map);
     geoTweet.bind();
@@ -31,17 +31,74 @@ var GeoTweet = (function () {
         $historyBtn = $('#history-btn');
     }
 
+    function getQuery() {
+        return $input.val();
+    }
+
     /**
      * Bind click event on Search button
      */
     function bindSearchBtn() {
         $searchBtn.off().on('click', function(e) {
             e.preventDefault();
+            performSearch(getQuery());
         });
     }
 
-    function search(query, successCallback, failureCallback) {
+    /**
+     * Bind keydown event on query input, capture for Return
+     */
+    function bindQueryInputKeyup() {
+        $input.on('keyup', function(e) {
+            if (e.keyCode == 13) {
+                e.preventDefault();
+                performSearch(getQuery());
+            }
+        });
+    }
 
+    /**
+     * Perform a search with query. On success, zoom to city location
+     * and display tweets on map
+     * @param query
+     */
+    function performSearch(query) {
+        if (query.length > 0) {
+            search(query, function(data) {
+                var city = data.city, tweets = data.tweets;
+                mapPanAndZoom(city, 13);
+            });
+        }
+    }
+
+    /**
+     * Center map to a city and zoom
+     * @param city
+     * @param zoom
+     */
+    function mapPanAndZoom(city, zoom) {
+        var coords = {lat: parseFloat(city.latitude), lng: parseFloat(city.longitude)};
+        map.panTo(coords);
+        if (typeof zoom == 'number') map.setZoom(zoom);
+    }
+
+    /**
+     * Perform a search with query
+     * @param query
+     * @param successCallback
+     * @param failureCallback
+     */
+    function search(query, successCallback, failureCallback) {
+        var apiUrl = '/tweet';
+        $.getJSON(apiUrl, {query: encodeURIComponent(query)}, function(response) {
+            if (response && response.errors.length == 0) {
+                // if success
+                if (typeof successCallback == 'function') successCallback(response.data);
+            } else {
+                // if something goes wrong
+                if (typeof failureCallback == 'function') failureCallback(response.errors);
+            }
+        });
     }
 
     /**
@@ -49,6 +106,7 @@ var GeoTweet = (function () {
      */
     GeoTweet.prototype.bind = function () {
         bindSearchBtn();
+        bindQueryInputKeyup();
     };
 
     return GeoTweet;
