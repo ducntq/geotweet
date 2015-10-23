4e3fe415a71b9e610bc23df5f814a78b7162c5c3
Duc Nguyen <ducntq@gmail.com>
2015-10-23 23:16:27 +0700

    Improve title position on mobile device

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var elixir = require('laravel-elixir');
 process.env.DISABLE_NOTIFIER = true;
+
+var elixir = require('laravel-elixir');
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
